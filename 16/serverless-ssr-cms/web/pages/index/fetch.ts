afc2ab6da937eae10f86fabbe7f1d292f5361a18
nodejh <jianghangscu@gmail.com>
2021-01-28 01:24:53 +0800

    feat: remove useless codes

--- 16/serverless-ssr-cms/web/pages/index/fetch.ts
+++ 16/serverless-ssr-cms/web/pages/index/fetch.ts
@@ -9,7 +9,6 @@ export default async (ctx: IFaaSContext<{
 }>) => {
   const data = __isBrowser__ ? await (await window.fetch('/api/index')).json() : await ctx.apiService?.index()
   return {
-    // 建议根据模块给数据加上 namespace防止数据覆盖
     indexData: data
   }
 }
